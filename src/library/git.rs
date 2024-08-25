061df6997546adcdd730af9168232cf561b571ea
Mark <04x0xx@gmail.com>
2024-08-24 23:23:56 -0300

    fefactor: Changed how creating a new repo works

--- src/library/git.rs
+++ src/library/git.rs
@@ -39,71 +39,112 @@ pub fn fetch_repo(ssh_url: &str, out_dir: &Path, args: Arc<CliArgs>) -> Result<R
     });
     */
 
-    // Prepares fetch options
-    let mut fo = git2::FetchOptions::new();
-    fo.remote_callbacks(callbacks);
-    fo.depth(args.clone_depth as i32); // Sets max depth to the repo clone
+    let repo = match out_dir.is_dir() {
+        true => Repository::open(out_dir).with_context(|| "Can't find the repo directory (do you have the right path?)")?,
+        false => {
 
-    // Prepare builder.
-    let mut builder = git2::build::RepoBuilder::new();
-    builder.fetch_options(fo);
+            // Prepares fetch options
+            let mut fo = git2::FetchOptions::new();
+            fo.remote_callbacks(callbacks);
+            fo.depth(args.clone_depth as i32); // Sets max depth to the repo clone
 
-    // Clones the repo
-    if out_dir.is_dir() {
-        debug!("Updating Index...");
-        let repo = Repository::open(out_dir).with_context(|| "Can't find the repo directory (do you have the right path?)")?;
+            // Prepare builder.
+            let mut builder = git2::build::RepoBuilder::new();
+            builder.fetch_options(fo);
+            builder.bare(true);
 
-        let refs = repo.references_glob("refs/remotes/*/*").unwrap();
-        for refname in refs {
-            let oid = repo.refname_to_id(refname.unwrap().name().unwrap()).unwrap();
-            let object = repo.find_object(oid, None).unwrap();
-            repo.reset(&object, git2::ResetType::Hard, None).unwrap();
-        }
-        debug!("Updating Refs...");
+            debug!("Cloning for the first time...");
 
-        let remotes = repo.remotes()?;
-        debug!("Found remotes: {:?}",
-            remotes.iter().map(|v| v.unwrap()).collect::<Vec<&str>>()
-        );
+            let git_out_dir = out_dir.join(".git/");
+            debug!("Cloning into: {git_out_dir:?}");
+            let _ = std::fs::create_dir_all(&git_out_dir).with_context(|| "Can't create directory!")?;
+            let repo = Repository::init_bare(git_out_dir).with_context(|| "Can't initialize repo")?;
 
-        let branches = repo.branches(None)?
-            .map(|branch_res| {
-                let (branch, _branch_type) = branch_res.unwrap();
-                branch.name().unwrap().unwrap().to_string()
-            })
-            .collect::<Vec<String>>();
-        debug!("Found branches: {branches:?}");
+            // let repo = builder.clone(ssh_url, &git_out_dir).with_context(|| format!("Can't clone repo to `{out_dir:?}` (do you have the right URL?)"))?;
 
-        for remote_str in remotes.iter() {
-            let mut remote = repo.find_remote(remote_str.unwrap()).unwrap();
+            // Sets some config rules
+            let mut config = repo.config()?;
 
-            let ref_specs_raw = remote.fetch_refspecs().unwrap();
-            let ref_specs = ref_specs_raw
-                .iter().map(|v| { v.unwrap() })
-                .collect::<Vec<&str>>()
-                ;
+            // Makes repo not bare
+            config.set_bool("core.bare", false)?;
 
-            let _ = match remote.fetch(&ref_specs, None, None) {
-                Ok(_) => (),
-                Err(v) => {
-                    debug!(
-                        "Can't clone from remote: {:?} with refspecs: {:?}. Error: {v:?}",
-                        remote.name(),
-                        ref_specs
-                        );
+            // Sets remote rules
+            config.set_str( "remote.origin.partialclonefilter", "blob:limit=1")?;
+            config.set_bool("remote.origin.promisor", true)?;
+
+            // Sets remotes
+            let _remote = match repo.remote("origin", ssh_url) {
+                Ok(v) => {
+                    debug!("Added remote: {}", v.name().unwrap());
+                    ()
                 },
+                Err(_e) => {
+                    debug!("Couldn't add remote!");
+                    ()
+                }
             };
 
-            debug!("Fetching Updates...");
+            // Reports configuration
+            let mut entries = config.entries(None)?;
 
+            while let Some(entry) = entries.next() {
+                let res = entry.unwrap();
+                debug!("Config Entry: {} = {}", res.name().unwrap(), res.value().unwrap());
+            }
+
+            repo
         }
+    };
 
-        return Ok(repo);
+    // Updates the repo
+    debug!("Updating Index...");
 
-    } else {
-        debug!("Cloning for the first time...");
-        return Ok(builder.clone(ssh_url, out_dir).with_context(|| format!("Can't clone repo to `{out_dir:?}` (do you have the right URL?)"))?);
+    let refs = repo.references_glob("refs/remotes/*/*").unwrap();
+    for refname in refs {
+        let oid = repo.refname_to_id(refname.unwrap().name().unwrap()).unwrap();
+        let object = repo.find_object(oid, None).unwrap();
+        repo.reset(&object, git2::ResetType::Hard, None).unwrap();
     }
+    debug!("Updating Refs...");
+
+    let remotes = repo.remotes()?;
+    debug!("Found remotes: {:?}",
+        remotes.iter().map(|v| v.unwrap()).collect::<Vec<&str>>()
+    );
+
+    let branches = repo.branches(None)?
+        .map(|branch_res| {
+            let (branch, _branch_type) = branch_res.unwrap();
+            branch.name().unwrap().unwrap().to_string()
+        })
+        .collect::<Vec<String>>();
+    debug!("Found branches: {branches:?}");
+
+    for remote_str in remotes.iter() {
+        let mut remote = repo.find_remote(remote_str.unwrap()).unwrap();
+
+        let ref_specs_raw = remote.fetch_refspecs().unwrap();
+        let ref_specs = ref_specs_raw
+            .iter().map(|v| { v.unwrap() })
+            .collect::<Vec<&str>>()
+            ;
+
+        let _ = match remote.fetch(&ref_specs, None, None) {
+            Ok(_) => (),
+            Err(v) => {
+                debug!(
+                    "Can't clone from remote: {:?} with refspecs: {:?}. Error: {v:?}",
+                    remote.name(),
+                    ref_specs
+                    );
+            },
+        };
+
+        debug!("Fetching Updates...");
+
+    }
+
+    return Ok(repo);
 
 }
 
