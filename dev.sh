0e0a2316b575e520a333747e34d224b253f141ad
some1and2-xc <04x0xx@gmail.com>
2024-10-09 16:15:02 -0300

    docs: Added install instruction to `dev.sh`

--- dev.sh
+++ dev.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/sh
 # Runs the development script to live reload
 # Uses: https://github.com/watchexec/watchexec
+# This can be installed through cargo with
+# ```sh
+# cargo install --locked watchexec-cli
+# ```
 
 watchexec -w src -r cargo run -- --log DEBUG
